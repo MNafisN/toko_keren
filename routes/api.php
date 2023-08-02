2b96d2077b4c07f091a284991a3b17bca2b798d3
MNafisN <naf1s.n4ufally@gmail.com>
2023-08-02 21:50:18 +0700

    revisi API user komplit

--- routes/api.php
+++ routes/api.php
@@ -24,19 +24,19 @@ use App\Http\Controllers\PenjualanController;
 Route::group([
     'prefix' => 'user'
 ], function() {
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);                       // Register user
+    Route::post('login', [AuthController::class, 'login']);                             // Login user
     Route::group([
         'middleware' => 'auth:api'
     ], function() {
-        Route::post('logout', [AuthController::class, 'logout']);
-        Route::post('refresh', [AuthController::class, 'refresh']);
-        Route::get('data', [AuthController::class, 'data']);
-        Route::post('update', [AuthController::class, 'update']);
-        Route::post('update_email', [AuthController::class, 'updateEmail']);
-        Route::post('update_password', [AuthController::class, 'updatePassword']);
-        Route::post('upload_photo', [AuthController::class, 'uploadPhoto']);
-        Route::delete('delete_photo', [AuthController::class, 'deletePhoto']);
+        Route::post('logout', [AuthController::class, 'logout']);                       // Logout user
+        Route::post('refresh', [AuthController::class, 'refresh']);                     // Refresh user token (180 minutes)
+        Route::get('data', [AuthController::class, 'data']);                            // Get user detail
+        Route::put('update', [AuthController::class, 'update']);                        // Update user data
+        Route::put('update_email', [AuthController::class, 'updateEmail']);             // Change user email
+        Route::put('update_password', [AuthController::class, 'updatePassword']);       // Change user password
+        Route::post('upload_photo', [AuthController::class, 'uploadPhoto']);            // Upload user profile picture
+        Route::put('delete_photo', [AuthController::class, 'deletePhoto']);             // Remove user profile picture
     });
 });
 
